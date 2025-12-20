8076f06422e4e68cb4ef6c0ef9462aadd223103e
unknown <Ishank>
2025-12-21 01:01:49 +0530

    HyperParmeter Tuning

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformation
 from src.mlproject.components.data_transformation import DataTransformationConfig
 import sys
-
+from src.mlproject.components.model_trainer import ModelTrainer, ModelTrainerConfig
 if __name__ == "__main__":
     logging.info("The Execution has started")
 
@@ -15,8 +15,10 @@ if __name__ == "__main__":
         train_data_path,test_data_path = data_ingestion.initiate_data_ingestion()
         # data_transformation_config = DataTransformationConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-
+        train_arr,test_arr,_ =data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        # Model Trainer
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
